d83da5a6a51e5fa743951ea2f35e3d72d8fb2967
edongjoselito <143322244+edongjoselito@users.noreply.github.com>
2026-09-07 09:11:14 +0800

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'https://localhost/ap-lead/';
+$config['base_url'] = 'http://localhost/ap-lead/';
 
 /*
 |--------------------------------------------------------------------------
